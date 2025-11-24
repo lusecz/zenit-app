bb6a53c725e460c35a7dff077fe381f321b0e1c7
Philip Escudero <sousaphilip32@gmail.com>
2025-11-24 16:32:45 -0300

    alteracao na barra de navegacao

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,48 +1,14 @@
 import { Tabs } from "expo-router";
-import React from "react";
-import { Platform } from "react-native";
-
-import { HapticTab } from "@/components/HapticTab";
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import TabBarBackground from "@/components/ui/TabBarBackground";
-import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: { position: "absolute" },
-          default: {},
-        }),
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="routines"
-        options={{
-          title: "Rotinas",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="dumbbell.fill" color={color} />
-          ),
-        }}
-      />
+    <Tabs screenOptions={{ 
+      headerShown: false,
+      tabBarStyle: { display: "none" }   // <<< oculta a TabBar
+    }}>
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="routines" />
+      <Tabs.Screen name="history" />
     </Tabs>
   );
 }
